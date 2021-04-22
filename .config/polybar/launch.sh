8b642ced4752ae1a0863a0c795ba3decbc2e47bd
Ahmed Mohamed <52778711+ahmedmohamed-hue@users.noreply.github.com>
2021-04-22 17:13:47 +0300

    polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,1 +1,31 @@
+#!/bin/bash
 
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# if type "xrandr"; then
+#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#     MONITOR=$m polybar --reload bar &
+#   done
+# else
+#   polybar --reload bar &
+# fi
+
+BAR_NAME=bar
+
+PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+
+# Launch on primary monitor
+MONITOR=$PRIMARY polybar --reload $BAR_NAME &
+sleep 1
+
+# Launch on all other monitors
+for m in $OTHERS; do
+ MONITOR=$m polybar --reload $BAR_NAME &
+done
+
+echo "Polybar launched..."
